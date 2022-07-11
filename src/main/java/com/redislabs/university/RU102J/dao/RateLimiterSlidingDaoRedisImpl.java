09f7064e5e34b705417e93c9fee63c91ce4982be
Jose A. I. López <17472377+J-A-I-L@users.noreply.github.com>
2022-07-11 23:57:46 +0200

    Challenge #7 Rate limiter Implement `hit()` in `RateLimiterSlidingDaoRedisImpl.java`.

--- src/main/java/com/redislabs/university/RU102J/dao/RateLimiterSlidingDaoRedisImpl.java
+++ src/main/java/com/redislabs/university/RU102J/dao/RateLimiterSlidingDaoRedisImpl.java
@@ -1,6 +1,9 @@
 package com.redislabs.university.RU102J.dao;
 
-import redis.clients.jedis.JedisPool;
+import com.redislabs.university.RU102J.core.KeyHelper;
+import redis.clients.jedis.*;
+
+import java.util.UUID;
 
 public class RateLimiterSlidingDaoRedisImpl implements RateLimiter {
 
@@ -19,6 +22,23 @@ public class RateLimiterSlidingDaoRedisImpl implements RateLimiter {
     @Override
     public void hit(String name) throws RateLimitExceededException {
         // START CHALLENGE #7
+        try (Jedis jedis = jedisPool.getResource()) {
+            String keyName = "limiter" + ":" + windowSizeMS + ":" + name + ":" + maxHits;
+            String key = KeyHelper.getKey(keyName);
+
+            final Transaction transaction = jedis.multi();
+            final long currentTimeStamp = System.currentTimeMillis();
+            final UUID uuid = UUID.randomUUID();
+            final String member = "" + currentTimeStamp + "-" + uuid;
+            transaction.zadd(key, currentTimeStamp, member);
+            transaction.zremrangeByScore(key,0,  currentTimeStamp - windowSizeMS);
+            final Response<Long> hits = transaction.zcard(key);
+            transaction.exec();
+
+            if (hits.get() > maxHits) {
+                throw new RateLimitExceededException();
+            }
+        }
         // END CHALLENGE #7
     }
 }
